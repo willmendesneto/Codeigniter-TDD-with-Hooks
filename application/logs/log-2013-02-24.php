1e5c53d025e1a156b1a754f4b930da4a8459a034
willmendesneto <willmendesneto@gmail.com>
2013-02-24 17:01:30 -0300

    Inserting composer packages and create Post model
    Signed-off-by: willmendesneto <willmendesneto@gmail.com>

---
+++ application/logs/log-2013-02-24.php
@@ -0,0 +1,67 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+DEBUG - 2013-02-24 16:51:37 --> Config Class Initialized
+DEBUG - 2013-02-24 16:51:37 --> Hooks Class Initialized
+DEBUG - 2013-02-24 16:51:37 --> Utf8 Class Initialized
+DEBUG - 2013-02-24 16:51:37 --> UTF-8 Support Enabled
+DEBUG - 2013-02-24 16:51:37 --> URI Class Initialized
+DEBUG - 2013-02-24 16:51:37 --> Router Class Initialized
+DEBUG - 2013-02-24 16:51:37 --> No URI present. Default controller set.
+DEBUG - 2013-02-24 16:51:37 --> Output Class Initialized
+DEBUG - 2013-02-24 16:51:37 --> Security Class Initialized
+DEBUG - 2013-02-24 16:51:37 --> Input Class Initialized
+DEBUG - 2013-02-24 16:51:37 --> Global POST and COOKIE data sanitized
+DEBUG - 2013-02-24 16:51:37 --> Language Class Initialized
+DEBUG - 2013-02-24 16:51:37 --> Loader Class Initialized
+DEBUG - 2013-02-24 16:51:37 --> Controller Class Initialized
+DEBUG - 2013-02-24 16:51:37 --> File loaded: ../application/views/welcome_message.php
+DEBUG - 2013-02-24 16:51:37 --> Database Driver Class Initialized
+ERROR - 2013-02-24 16:51:37 --> Severity: Warning  --> Cannot modify header information - headers already sent by (output started at D:\projects\ci-tdd-hooks\vendor\phpunit\phpunit\PHPUnit\Util\Printer.php:173) D:\projects\ci-tdd-hooks\system\core\Common.php 521
+DEBUG - 2013-02-24 16:54:42 --> Config Class Initialized
+DEBUG - 2013-02-24 16:54:42 --> Hooks Class Initialized
+DEBUG - 2013-02-24 16:54:42 --> Utf8 Class Initialized
+DEBUG - 2013-02-24 16:54:42 --> UTF-8 Support Enabled
+DEBUG - 2013-02-24 16:54:42 --> URI Class Initialized
+DEBUG - 2013-02-24 16:54:42 --> Router Class Initialized
+DEBUG - 2013-02-24 16:54:42 --> No URI present. Default controller set.
+DEBUG - 2013-02-24 16:54:42 --> Output Class Initialized
+DEBUG - 2013-02-24 16:54:42 --> Security Class Initialized
+DEBUG - 2013-02-24 16:54:42 --> Input Class Initialized
+DEBUG - 2013-02-24 16:54:42 --> Global POST and COOKIE data sanitized
+DEBUG - 2013-02-24 16:54:42 --> Language Class Initialized
+DEBUG - 2013-02-24 16:54:42 --> Loader Class Initialized
+DEBUG - 2013-02-24 16:54:42 --> Controller Class Initialized
+DEBUG - 2013-02-24 16:54:42 --> File loaded: ../application/views/welcome_message.php
+DEBUG - 2013-02-24 16:54:42 --> Database Driver Class Initialized
+DEBUG - 2013-02-24 16:55:05 --> Config Class Initialized
+DEBUG - 2013-02-24 16:55:05 --> Hooks Class Initialized
+DEBUG - 2013-02-24 16:55:05 --> Utf8 Class Initialized
+DEBUG - 2013-02-24 16:55:05 --> UTF-8 Support Enabled
+DEBUG - 2013-02-24 16:55:05 --> URI Class Initialized
+DEBUG - 2013-02-24 16:55:05 --> Router Class Initialized
+DEBUG - 2013-02-24 16:55:05 --> No URI present. Default controller set.
+DEBUG - 2013-02-24 16:55:05 --> Output Class Initialized
+DEBUG - 2013-02-24 16:55:05 --> Security Class Initialized
+DEBUG - 2013-02-24 16:55:05 --> Input Class Initialized
+DEBUG - 2013-02-24 16:55:05 --> Global POST and COOKIE data sanitized
+DEBUG - 2013-02-24 16:55:05 --> Language Class Initialized
+DEBUG - 2013-02-24 16:55:05 --> Loader Class Initialized
+DEBUG - 2013-02-24 16:55:05 --> Controller Class Initialized
+DEBUG - 2013-02-24 16:55:05 --> File loaded: ../application/views/welcome_message.php
+DEBUG - 2013-02-24 16:55:05 --> Database Driver Class Initialized
+DEBUG - 2013-02-24 16:55:20 --> Config Class Initialized
+DEBUG - 2013-02-24 16:55:20 --> Hooks Class Initialized
+DEBUG - 2013-02-24 16:55:20 --> Utf8 Class Initialized
+DEBUG - 2013-02-24 16:55:20 --> UTF-8 Support Enabled
+DEBUG - 2013-02-24 16:55:20 --> URI Class Initialized
+DEBUG - 2013-02-24 16:55:20 --> Router Class Initialized
+DEBUG - 2013-02-24 16:55:20 --> No URI present. Default controller set.
+DEBUG - 2013-02-24 16:55:20 --> Output Class Initialized
+DEBUG - 2013-02-24 16:55:20 --> Security Class Initialized
+DEBUG - 2013-02-24 16:55:20 --> Input Class Initialized
+DEBUG - 2013-02-24 16:55:20 --> Global POST and COOKIE data sanitized
+DEBUG - 2013-02-24 16:55:20 --> Language Class Initialized
+DEBUG - 2013-02-24 16:55:20 --> Loader Class Initialized
+DEBUG - 2013-02-24 16:55:20 --> Controller Class Initialized
+DEBUG - 2013-02-24 16:55:20 --> File loaded: ../application/views/welcome_message.php
+DEBUG - 2013-02-24 16:55:20 --> Database Driver Class Initialized
