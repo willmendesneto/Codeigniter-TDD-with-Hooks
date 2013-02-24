1645f1daf0efc25aa9cd954f0b98fd4a3e9f89b1
willmendesneto <willmendesneto@gmail.com>
2013-02-24 16:21:51 -0300

    Configuring the database envirounments
    Signed-off-by: willmendesneto <willmendesneto@gmail.com>

--- application/config/database.php
+++ application/config/database.php
@@ -82,10 +82,10 @@ $query_builder = TRUE;
 $db['default'] = array(
 	'dsn'	=> '',
 	'hostname' => 'localhost',
-	'username' => '',
+	'username' => 'root',
 	'password' => '',
-	'database' => '',
-	'dbdriver' => 'mysqli',
+	'database' => 'ci-phpunit',
+	'dbdriver' => 'mysql',
 	'dbprefix' => '',
 	'pconnect' => TRUE,
 	'db_debug' => TRUE,
@@ -101,5 +101,26 @@ $db['default'] = array(
 	'failover' => array()
 );
 
+$db['testing'] = array(
+	'dsn'	=> '',
+	'hostname' => 'localhost',
+	'username' => 'root',
+	'password' => '',
+	'database' => 'ci-phpunit',
+	'dbdriver' => 'mysql',
+	'dbprefix' => '',
+	'pconnect' => TRUE,
+	'db_debug' => TRUE,
+	'cache_on' => FALSE,
+	'cachedir' => '',
+	'char_set' => 'utf8',
+	'dbcollat' => 'utf8_general_ci',
+	'swap_pre' => '',
+	'autoinit' => TRUE,
+	'encrypt' => FALSE,
+	'compress' => FALSE,
+	'stricton' => FALSE,
+	'failover' => array()
+);
 /* End of file database.php */
 /* Location: ./application/config/database.php */
